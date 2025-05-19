277d56b771efb6edd8b352dd9a571b3bdee1632f
Rustam Akhmadullin <ru.akhmadullin@yadro.com>
2025-05-19 11:29:10 +0300

    add logger

--- src/sl_demo/main.py
+++ src/sl_demo/main.py
@@ -2,13 +2,18 @@ from saleae import automation
 import os
 import os.path
 from datetime import datetime
+from loguru import logger
 
+
+@logger.catch
 def main():
     # Connect to the running Logic 2 Application on port `10430`.
     # Alternatively you can use automation.Manager.launch() to launch a new Logic 2 process - see
     # the API documentation for more details.
     # Using the `with` statement will automatically call manager.close() when exiting the scope. If you
     # want to use `automation.Manager` outside of a `with` block, you will need to call `manager.close()` manually.
+    logger.add(f'logs/{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log')
+    logger.info("start connection")
     with automation.Manager.connect(port=10430) as manager:
 
         # Configure the capturing device to record on digital channels 0, 1, 2, and 3,
@@ -31,6 +36,7 @@ def main():
         #         1. Omit the `device_id` argument. Logic 2 will choose the first real (non-simulated) device.
         #         2. Use the serial number for your device. See the "Finding the Serial Number
         #            of a Device" section for information on finding your device's serial number.
+        logger.info('start capture')
         with manager.start_capture(
                 device_id='A7D1BB81883C0092',
                 device_configuration=device_configuration,
@@ -58,13 +64,17 @@ def main():
                 filepath=analyzer_export_filepath,
                 analyzers=[serial_analyzer]
             )
+            logger.info(f'Export analyzer data to a CSV file {analyzer_export_filepath=}')
 
             # Export raw digital data to a CSV file
             capture.export_raw_data_csv(directory=output_dir, digital_channels=[0, 2])
+            logger.info(f'Export raw digital data to a CSV file {output_dir=}')
 
             # Finally, save the capture to a file
             capture_filepath = os.path.join(output_dir, 'example_capture.sal')
             capture.save_capture(filepath=capture_filepath)
+            logger.info(f'Finally, save the capture to a file {capture_filepath=}')
+
 
 if __name__ == '__main__':
     main()
