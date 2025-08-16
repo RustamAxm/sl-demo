9209c79e89056a96c0db7cab60be12b8897199b7
Rustam Akhmadullin <ru.akhmadullin@yadro.com>
2025-08-16 23:10:11 +0300

    add demo

--- src/sl_demo/analyser.py
+++ src/sl_demo/analyser.py
@@ -7,8 +7,8 @@ from loguru import logger
 
 @dataclass
 class Range:
-    min_: str
-    max_: str
+    min_: datetime
+    max_: datetime
 
 
 @dataclass
@@ -20,11 +20,28 @@ class Timings:
 class Analyser:
     def __init__(self):
         self.start_time = None
+        self.dataframe = None
 
     def set_digital_csv(self, digital_csv):
         with open(digital_csv, 'r') as f:
-            data = pd.read_csv(f)
+            dataframe = pd.read_csv(f)
 
-        self.start_time = datetime.fromisoformat(data['Time [s]'][0])
+        self.start_time = self.convert_to_datetime(dataframe['Time [s]'][0])
         logger.debug(f"{self.start_time=}, {type(self.start_time)}")
-        logger.debug(self.start_time > datetime.fromisoformat(data['Time [s]'][1]))
+        logger.debug(self.start_time > datetime.fromisoformat(dataframe['Time [s]'][1]))
+        tmp = self.add_datetime_column(dataframe)
+        dataframe['delta_time'] = tmp
+        self.dataframe = dataframe
+        logger.debug(f"\n{dataframe}")
+
+    @staticmethod
+    def convert_to_datetime(dt_):
+        return datetime.fromisoformat(dt_)
+
+    def add_datetime_column(self, data):
+        tmp = []
+        for item in data['Time [s]']:
+            delta = self.convert_to_datetime(item) - self.start_time
+            tmp.append(delta)
+        logger.debug(f"{tmp=}")
+        return tmp
