4785eac6b107bf036b654a4ea7840728568ac305
Rustam Akhmadullin <ru.akhmadullin@yadro.com>
2025-08-16 22:39:26 +0300

    add demo

--- src/sl_demo/main.py
+++ src/sl_demo/main.py
@@ -1,9 +1,12 @@
+import os
 import time
 from datetime import datetime
 from loguru import logger
 from sl_demo import Manager
+from src.sl_demo.analyser import Analyser
 
-@logger.catch
+
+# @logger.catch
 def main():
     """
     # Connect to the running Logic 2 Application on port `10430`.
@@ -16,17 +19,30 @@ def main():
 
     logger.add(f'logs/{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log')
     logger.info("start connection")
-
+    iter = 1
+    # demo get vals
     with Manager() as dev:
-        dev.configuration(
-            duration_seconds=2,
-        )
         for i in range(3):
+            dev.configuration(
+                duration_seconds=iter,
+            )
+            iter += 1
             dev.start_capture_th()
             while dev.get_capture_th_status():
                 logger.info('in capture')
                 time.sleep(0.2)
             dev.analyse_and_save()
+    # load from csv
+    al = Analyser()
+    al.set_digital_csv(
+        digital_csv='output-2025-08-16_22-24-12/digital.csv',
+    )
+    # load special sale data
+    dev = Manager()
+    dev.load_capture(os.path.abspath('output-2025-08-16_22-24-12/example_capture.sal'))
+    dev.analyse_and_save()
+    dev.close()
+
 
 if __name__ == '__main__':
     main()
