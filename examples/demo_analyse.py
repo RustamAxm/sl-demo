03a9f05dbd6689c1008be2850ddc6de7b6129bf2
Rustam Akhmadullin <ru.akhmadullin@yadro.com>
2025-08-17 13:53:03 +0300

    add filtered time

--- examples/demo_analyse.py
+++ examples/demo_analyse.py
@@ -1,7 +1,15 @@
+from loguru import logger
 from matplotlib import pyplot as plt
 
 from src.sl_demo.analyser import Analyser
 
+def plot_df(df):
+    plt.figure(figsize=(10, 6))
+    plt.plot(df['Time [s]'], df['ChannelA 0'], label='D', marker='o', linestyle='-', color='red')
+    plt.plot(df['Time [s]'], df['Channel 0'], label='A', marker='s', linestyle='--', color='blue')
+    plt.title('График данных')
+    plt.grid(True)
+    plt.show()
 
 def main():
     # load from csv
@@ -15,12 +23,10 @@ def main():
     al.merge_dataframes()
 
     df = al.get_all_dataframe_resample()
-    plt.figure(figsize=(10, 6))
-    plt.plot(df['Time [s]'], df['ChannelA 0'], label='D', marker='o', linestyle='-', color='red')
-    plt.plot(df['Time [s]'], df['Channel 0'], label='A', marker='s', linestyle='--', color='blue')
-    plt.title('График данных')
-    plt.grid(True)
-    plt.show()
+
+    start = '2025-08-17T07:59:59.899+00:00'
+    end = '2025-08-17T07:59:59.900000000+00:00'
+    logger.info(f"{al.get_filtered_time(start, end)}")
 
 
 if __name__ == '__main__':
