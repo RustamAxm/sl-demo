be84c427d125d38f1f829841e787d1a2980090d9
rustam <rustamaxm@gmail.com>
2025-08-21 23:04:24 +0300

    add demo

---
+++ examples/demo_analyse.py
@@ -0,0 +1,13 @@
+from src.sl_demo.analyser import Analyser
+
+
+def main():
+    # load from csv
+    al = Analyser()
+    al.set_digital_csv(
+        digital_csv='output-2025-08-16_22-48-44/digital.csv',
+    )
+
+
+if __name__ == '__main__':
+    main()
